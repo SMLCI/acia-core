477f107c82a54d16e1e63215d6ee812ee8bce907
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2025-10-27 09:42:02 +0100

    Remove old examples

--- omero_examples/loadOmeroRoIs.py
+++
@@ -1,8 +0,0 @@
-"""Example to load omero RoIs for an image sequence"""
-
-
-from acia.segm.omero.storer import OmeroRoIStorer
-
-overlay = OmeroRoIStorer.load(2, "root", "omero", "ibt056")
-
-print("Num contours", len(overlay.contours))
