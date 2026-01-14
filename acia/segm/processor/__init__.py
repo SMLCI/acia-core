fc5855310af8ce3506649c09774dd4f05cb947fd
JojoDevel <14841215+JojoDevel@users.noreply.github.com>
2026-01-14 16:29:12 +0100

    Fix import

--- acia/segm/processor/__init__.py
+++ acia/segm/processor/__init__.py
@@ -8,3 +8,9 @@ class SegmentationProcessor:
 
     def __call__(self, images: ImageSequenceSource) -> Overlay:
         raise NotImplementedError("Please implement this base function")
+
+
+# Import subclasses after base class is defined to avoid circular imports
+from acia.segm.processor.canny import (  # noqa: E402
+    CannySegmentationProcessor as CannySegmentationProcessor,
+)
